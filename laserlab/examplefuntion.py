35d74630409bac542009039a4033c694b2d1d925
laserlab <ublaserlab@gmail.com>
2024-10-12 10:49:34 -0400

    added example for testing github actions

---
+++ laserlab/examplefuntion.py
@@ -0,0 +1,16 @@
+""" This is an example module for testing github actions"""
+import numpy as np
+
+def add(x,y):
+  """ simple adder
+  args:
+    x: first value
+    y: second value
+  return:
+    sum of both inputs
+"""
+  return x+y
+
+def test_add():
+  """ unit test for add function"
+  assert add(1,1) == 2
