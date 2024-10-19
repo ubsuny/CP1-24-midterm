a7d8ad8155f29c76b096e432400d82fe59e46c76
zbpetersbuf <zbpeters@buffalo.edu>
2024-10-19 16:04:31 +0000

    updatad code

--- zbpetersbuf/code/code_tobenamedlatter.py
+++ zbpetersbuf/code/code_tobenamedlatter.py
@@ -13,3 +13,11 @@ def imptomec(x,lenguni):
         x = 3*x
     return 0.305*x
 
+import pandas as pd
+df = pd.read_csv('/workspaces/CP1-24-midterm/zbpetersbuf/data/tes_data.csv')
+
+print(df)
+
+
+eupdata = pd.read_csv('/workspaces/CP1-24-midterm/zbpetersbuf/data/LL001_elevatorup.cvs')
+edowndata = pd.read_csv('/workspaces/CP1-24-midterm/zbpetersbuf/data/LL002_elevatordown.cvs')
