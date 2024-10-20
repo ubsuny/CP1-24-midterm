eb1511e30cb74c170ffc65582da3a9d2452962ff
iglesias-cardinale <112112966+iglesias-cardinale@users.noreply.github.com>
2024-10-20 01:51:23 +0000

    Added round earth correction to distance.py

--- iglesias-cardinale/code/distance.py
+++ iglesias-cardinale/code/distance.py
@@ -2,7 +2,9 @@
 This module contains functions to find distances between two GPS positions
 """
 
-def gps_flat_earth(x1,x2):
+import numpy as np
+
+def gps_distance_flat_earth(x1,x2):
     '''
     Calculates the absolute distance between two adjacent gps points assuming a flat Earth
 
@@ -17,3 +19,68 @@ def gps_flat_earth(x1,x2):
     '''
 
     return (sum((x2[i]-x1[i])**2 for i in len(x1)))**0.5
+
+#Useful constants
+
+#Radii of Earth using the WGS84 ellipsoid (as used in phyphox), according to
+#https://gscommunitycodes.usf.edu/geoscicommunitycodes/public/geophysics/
+R_EQUATOR = 6378137 #Meters
+R_POLE = 6356752 #Meters
+
+#Flattening
+FLAT = (R_EQUATOR - R_POLE)/R_EQUATOR
+
+def r_earth(phi):
+    '''
+    Calculates the radius of the earth in the WGS84 ellipsoid at a givel latitude
+    
+    Parameters: 
+    - phi: Angle of latitude at which we are finding the radius
+    
+    Returns:
+    - Radius of Earth at given latitude
+    '''
+    return R_EQUATOR*(1-FLAT*np.sin(phi)**2)
+
+def gps_wgs84(lat,lon,alt):
+    '''
+    Converts GPS position given by latitude, longitude and altiude using the WGS84 
+    ellipsoid into x, y and z coordinates
+
+    parameters:
+    - lat: latitude in radians
+    - lon: longitude in radians
+    - alt: altitude in meters in the WGS84 ellipsoid
+
+    returns:
+    - 
+
+    '''
+
+    r = r_earth(lat) + alt
+
+    x = r*np.sin(lon)*np.cos(lat)
+    y = r*np.sin(lon)*np.sin(lat)
+    z = r*np.cos(lon)
+
+    return x, y, z
+
+def gps_distance_wgs84(p1,p2):
+    '''
+    Calculates the distance between two points given by latitude, longitude and altitude 
+    using the WGS84 ellipsoid.
+
+    parameters:
+    - p1: List or array of the form [lat, lon, alt] for the first point
+    - p2: List or array of the form [lat, lon, alt] for the second point
+
+    returns:
+    - absolute distance between p1 and p2
+    '''
+
+    x1, y1, z1 = gps_wgs84(p1[0], p1[1], p1[2])
+    x2, y2, z2 = gps_wgs84(p2[0], p2[1], p2[2])
+
+    d = np.sqrt((x2-x1)**2+(y2-y1)**2+(z2-z1)**2)
+
+    return d
