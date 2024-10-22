176ee2d469cff089e914b438fa24969cc032b227
zbpetersbuf <zbpeters@buffalo.edu>
2024-10-22 16:31:31 +0000

    pylintedwalking

--- zbpetersbuf/code/ignor.py
+++ zbpetersbuf/code/ignor.py
@@ -1,43 +1,56 @@
+"""
+
+import matplotlib.pyplot as plt
+import os
+
+for the test the horizontal accuracy was 4.7 m, and verticle accuracy was 3.4
+"""
+
+
 import math as m
 import numpy as np
 import pandas as pd
 
 def walkeq(walkdt):
-    #walkt = list(walkdt.loc[:, 'Time (s)'])
-    laloalt = list(zip(walkdt.loc[:, 'Latitude (°)'], walkdt.loc[:, 'Longitude (°)'], walkdt.loc[:, 'Altitude WGS84 (m)']))
-    a = 6378000
-    f = 1/(298.3)
-    b=(1-f)*a
-    l=abs(laloalt[0][1]-laloalt[1][1])
+    """docstring type words here"""
+
+    laloalt = list(zip(walkdt.loc[:, 'Latitude (°)'],
+                        walkdt.loc[:, 'Longitude (°)'], walkdt.loc[:, 'Altitude WGS84 (m)']))
+
+    u = np.zeros(walkdt.shape[0])
     s = np.zeros(walkdt.shape[0])
-    lam = l
 
-    
+    for i in range(walkdt.shape[0]):
+        u[i] = np.radians(m.atan((1-(np.radians(1/(298.257))))*m.tan(np.radians(laloalt[i][0]))))
+
     for i in range(walkdt.shape[0]-1):
-        phi=laloalt[i][0]
-        u[i] = m.atan((1-f)*m.tan(phi))
-
-        #lam = l
-        while lam > 10**(-5):
-            sio=((m.cos(u[i+1])*m.sin(lam))**2+(m.cos(u[i])*(m.sin(u[i+1]))-(m.sin(u[i]))*(m.cos(u[i+1]))*(m.cos(lam)))**2)**(1/2)
-            coo=(m.sin(u[i]))*(m.sin(u[i+1]))+(m.cos(u[i]))*(m.cos(u[i+1]))*(m.cos(lam))
-            sig=m.atan2(sio,coo)
+        lam = np.radians(abs(laloalt[0][1]-laloalt[1][1]))
+        lamp=0
+        while abs(abs(lam) - abs(lamp)) > 10**(-12):
+            lamp = lam
+            sig=m.atan2(((m.cos(u[i+1])*m.sin(lam))**2+(m.cos(u[i])*
+                    (m.sin(u[i+1]))-(m.sin(u[i]))*(m.cos(u[i+1]))*(m.cos(lam)))**2)**(1/2),
+                        (m.sin(u[i]))*(m.sin(u[i+1]))+(m.cos(u[i]))*(m.cos(u[i+1]))*(m.cos(lam)))
             alp=m.asin((m.cos(u[i])*(m.cos(u[i+1]))*(m.sin(lam)))/(m.sin(sig)))
             sigm=0.5*m.acos(m.cos(sig)-2*(m.sin(u[i])*(m.sin(u[i+1])))/(m.cos(alp)**2))
-            c=f*(m.cos(sig)**2)*(4+f*(4-3*(m.cos(alp)**2)))/16
-            lam=l+(1-c)*f*(m.sin(sig))*(sig+c*m.sin(sig)*(m.cos(2*sigm)+c*m.cos(sig)*(2*(m.cos(2*sigm)**2)-1)))
-        
-       
+            c=(np.radians(1/(298.257)))*(m.cos(sig)**2)*(4+(np.radians(1/(298.257)))*
+                    (4-3*(m.cos(alp)**2)))/16
+            lam=abs(laloalt[0][1]-laloalt[1][1])+(1-
+                    c)*(np.radians(1/(298.257)))*(m.sin(sig))*(sig+c*m.sin(sig)*
+                        (m.cos(2*sigm)+c*m.cos(sig)*(2*(m.cos(2*sigm)**2)-1)))
 
-        litu=((m.cos(alp))**2)*((a**2-b**2)/(b**2))
-        biga=1+((u[i]**2)/16384)*(4096+(litu**2)*((litu**2)*(320-175*(litu**2))-768))
-        bigb=((litu**2)/1024)*(256+(litu**2)*((litu**2)(74-47*(litu**2))-128))
-        dsf = bigb*m.sin(sig)
-        dsig=dsf*(m.cos(2*sigm)+0.25*bigb*(m.cos(sig)*(2*(m.cos(2*sigm))**2-1)-(1/6)*bigb*m.cos(2*sigm)*(4*(m.sin(sig)**2)-3)*(4*(m.cos(2*sigm)**2)-3)))
-        s[i+1]=b*biga*(sig-dsig)
+        litu=((m.cos(alp))**2)*(((np.radians(6378140))**2-((1-(np.radians(1
+                /(298.257))))*np.radians(6378140))**2)/(((
+                    1-(np.radians(1/(298.257))))*np.radians(6378140))**2))**2
+        biga=1+((u[i]**2)/16384)*(4096+(litu)*((litu)*(320-175*(litu))-768))
+        bigb=((litu)/1024)*(256+(litu)*((litu)*(74-47*(litu))-128))
+        dsig=(bigb*m.sin(sig))*(m.cos(2*sigm)+0.25*bigb*(m.cos(sig)*
+                (2*(m.cos(2*sigm))**2-1)-(1/6)*bigb*m.cos(2*sigm)*(4*
+                    (m.sin(sig)**2)-3)*(4*(m.cos(2*sigm)**2)-3)))
+        s[i+1]=((1-(np.radians(1/(298.257))))*np.radians(6378140))*biga*(sig-dsig)
 
     return s
 
-
-walk = pd.read_csv('/workspaces/CP1-24-midterm/zbpetersbuf/data/walktest.csv')
+walk = pd.read_csv('/workspaces/CP1-24-midterm/zbpetersbuf/data/LL07_circle.csv')
 print(walkeq(walk))
+print(sum(walkeq(walk)))
--- zbpetersbuf/code/workinh.py
+++ zbpetersbuf/code/workinh.py
@@ -6,52 +6,49 @@ import os
 for the test the horizontal accuracy was 4.7 m, and verticle accuracy was 3.4
 """
 
-
 import math as m
 import numpy as np
 import pandas as pd
 
 def walkeq(walkdt):
-    #walkt = list(walkdt.loc[:, 'Time (s)'])
-    laloalt = list(zip(walkdt.loc[:, 'Latitude (°)'], walkdt.loc[:, 'Longitude (°)'], walkdt.loc[:, 'Altitude WGS84 (m)']))
-    a = 6378000
-    f = 1/(298.3)
-    b=(1-f)*a
+    """docstring type words here"""
+
+    laloalt = list(zip(walkdt.loc[:, 'Latitude (°)'],
+                        walkdt.loc[:, 'Longitude (°)'], walkdt.loc[:, 'Altitude WGS84 (m)']))
+
     u = np.zeros(walkdt.shape[0])
-    phi = np.zeros(walkdt.shape[0])
-    l=abs(laloalt[0][1]-laloalt[1][1])
     s = np.zeros(walkdt.shape[0])
 
-    for i in range(walkdt.shape[0]-1):
-        phi[i]=np.radians(laloalt[i][0])
-        u[i] = np.radians(m.atan((1-f)*m.tan(phi[i])))
-    
-    for i in range(walkdt.shape[0]-1):
+    for i in range(walkdt.shape[0]):
+        u[i] = np.radians(m.atan((1-(np.radians(1/(298.257))))*m.tan(np.radians(laloalt[i][0]))))
 
-        lam = np.radians(l)
+    for i in range(walkdt.shape[0]-1):
+        lam = np.radians(abs(laloalt[0][1]-laloalt[1][1]))
         lamp=0
-        while abs(lam - lamp) > 10**(-10):
+        while abs(abs(lam) - abs(lamp)) > 10**(-12):
             lamp = lam
-            sio=((m.cos(u[i+1])*m.sin(lam))**2+(m.cos(u[i])*(m.sin(u[i+1]))-(m.sin(u[i]))*(m.cos(u[i+1]))*(m.cos(lam)))**2)**(1/2)
-            coo=(m.sin(u[i]))*(m.sin(u[i+1]))+(m.cos(u[i]))*(m.cos(u[i+1]))*(m.cos(lam))
-            sig=m.atan2(sio,coo)
+            sig=m.atan2(((m.cos(u[i+1])*m.sin(lam))**2+(m.cos(u[i])*
+                    (m.sin(u[i+1]))-(m.sin(u[i]))*(m.cos(u[i+1]))*(m.cos(lam)))**2)**(1/2),
+                        (m.sin(u[i]))*(m.sin(u[i+1]))+(m.cos(u[i]))*(m.cos(u[i+1]))*(m.cos(lam)))
             alp=m.asin((m.cos(u[i])*(m.cos(u[i+1]))*(m.sin(lam)))/(m.sin(sig)))
             sigm=0.5*m.acos(m.cos(sig)-2*(m.sin(u[i])*(m.sin(u[i+1])))/(m.cos(alp)**2))
-            c=f*(m.cos(sig)**2)*(4+f*(4-3*(m.cos(alp)**2)))/16
-            lam=l+(1-c)*f*(m.sin(sig))*(sig+c*m.sin(sig)*(m.cos(2*sigm)+c*m.cos(sig)*(2*(m.cos(2*sigm)**2)-1)))
-        
-       
+            c=(np.radians(1/(298.257)))*(m.cos(sig)**2)*(4+(np.radians(1/(298.257)))*
+                    (4-3*(m.cos(alp)**2)))/16
+            lam=abs(laloalt[0][1]-laloalt[1][1])+(1-
+                    c)*(np.radians(1/(298.257)))*(m.sin(sig))*(sig+c*m.sin(sig)*
+                        (m.cos(2*sigm)+c*m.cos(sig)*(2*(m.cos(2*sigm)**2)-1)))
 
-        litu=((m.cos(alp))**2)*((a**2-b**2)/(b**2))**2
+        litu=((m.cos(alp))**2)*(((np.radians(6378140))**2-((1-(np.radians(1
+                /(298.257))))*np.radians(6378140))**2)/(((
+                    1-(np.radians(1/(298.257))))*np.radians(6378140))**2))**2
         biga=1+((u[i]**2)/16384)*(4096+(litu)*((litu)*(320-175*(litu))-768))
         bigb=((litu)/1024)*(256+(litu)*((litu)*(74-47*(litu))-128))
-        dsf = bigb*m.sin(sig)
-        dsig=dsf*(m.cos(2*sigm)+0.25*bigb*(m.cos(sig)*(2*(m.cos(2*sigm))**2-1)-(1/6)*bigb*m.cos(2*sigm)*(4*(m.sin(sig)**2)-3)*(4*(m.cos(2*sigm)**2)-3)))
-        s[i+1]=b*biga*(sig-dsig)
+        dsig=(bigb*m.sin(sig))*(m.cos(2*sigm)+0.25*bigb*(m.cos(sig)*
+                (2*(m.cos(2*sigm))**2-1)-(1/6)*bigb*m.cos(2*sigm)*(4*
+                    (m.sin(sig)**2)-3)*(4*(m.cos(2*sigm)**2)-3)))
+        s[i+1]=((1-(np.radians(1/(298.257))))*np.radians(6378140))*biga*(sig-dsig)
+    return s
 
-    return s[1]
-
-
-walk = pd.read_csv('/workspaces/CP1-24-midterm/zbpetersbuf/data/walktest.csv')
-walk = pd.read_csv('/workspaces/CP1-24-midterm/zbpetersbuf/data/walktest.csv')
+walk = pd.read_csv('/workspaces/CP1-24-midterm/zbpetersbuf/data/LL07_circle.csv')
 print(walkeq(walk))
+print(sum(walkeq(walk)))
