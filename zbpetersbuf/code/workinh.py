9c92c494dca77865495692b6db208c4b994f9426
zbpetersbuf <zbpeters@buffalo.edu>
2024-10-24 14:45:17 +0000

    mes

--- zbpetersbuf/code/workinh.py
+++ zbpetersbuf/code/workinh.py
@@ -74,8 +74,8 @@ def gpsloc(datta):
     yax = np.zeros(datta.shape[0])
 
     for i in range(datta.shape[0]):
-        xax[i]=(laloalt[i][2])*(m.cos(np.radians(laloalt[i][0])))*(m.cos(np.radians(laloalt[i][1])))
-        yax[i]=(laloalt[i][2])*(m.cos(np.radians(laloalt[i][0])))*(m.sin(np.radians(laloalt[i][1])))
+        xax[i]=((laloalt[i][2]))*(m.cos((laloalt[i][0])/(m.pi/180)))*(m.cos((laloalt[i][1])/(m.pi/180)))
+        yax[i]=((laloalt[i][2]))*(m.cos((laloalt[i][0])/(m.pi/180)))*(m.sin((laloalt[i][1])/(m.pi/180)))
     
     return xax, yax
 
