f5016f6db82f848860be0cd7ba47701538ba086f
zbpetersbuf <zbpeters@buffalo.edu>
2024-10-24 00:09:03 +0000

    up

--- zbpetersbuf/code/imp_to_metr.py
+++ zbpetersbuf/code/imp_to_metr.py
@@ -4,10 +4,9 @@ imp_to_metr.py
 """
 def imptomec(x,lenguni):
     """enter your mesuremtn value and then the kind of mesurment, ie if feet enter ft, feet or foot
-    if its yards enter yards, yard or yd"""
+    if its yards enter yards, yard"""
     if lenguni == 'yard':
         x = 3*x
     return 0.305*x
 
-print(imptomec(1,'yard'))
-print(imptomec(1,'yards'))
+print((2023-1972)//4)
--- zbpetersbuf/code/uniti_con.py
+++ zbpetersbuf/code/uniti_con.py
@@ -1,16 +1,27 @@
 """doc string"""
-import matplotlib as m
-import numpy as np
-import pandas as pd
 import re
+import os
+import numpy as np
 
 def gt_unix(mod_name):
-    with open('/workspaces/CP1-24-midterm/zbpetersbuf/data/'mod_name, 'r') as f:
-        return np.delete(re.findall(r'\d+', f.read()),0)
+    """docstring"""
+    fpath='/workspaces/CP1-24-midterm/zbpetersbuf/data/'.strip()
+    filpath = os.path.join(fpath, mod_name)
 
+    with open(filpath, 'r', encoding='utf-8') as f:
+        ftime = np.delete(re.findall(r'\d+', f.read()),0)
 
+    matime = np.zeros(ftime.shape[0])
+    for i in range(ftime.shape[0]):
+        matime[i] = int(ftime[i])
+
+    awf = [0,31,60,91,121,152,182,213,244,274,305,335]
+
+    day = 365*(matime[0] - 1970) + matime[2] + awf[int(matime[1])] - max(0,((matime[0]-1972)//4))
+    minit = 60*(max(matime[3]-matime[7],0)) + matime[4]
+    sec = 86400*day + 60*minit + matime[5] + matime[6]*(10**(-3))
+
+    return sec
 
 asd=gt_unix('LL08_circle.md')
-
-
 print(asd)
