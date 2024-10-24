f546cc94d98f8fd16a89a651574aa692ef320600
HeshamElsaman <88336539+HeshamElsaman@users.noreply.github.com>
2024-10-24 14:57:37 +0000

    Modified the distance function

--- HeshamElsaman/Code/Algorithm/distance_functions.py
+++ HeshamElsaman/Code/Algorithm/distance_functions.py
@@ -11,7 +11,7 @@ longdiff, difference in longitude, positive east.
 s, length of the geodesic.
 alpha1, alpha2, azimuths of the geodesic, clockwise from north; alpha2 is the direction P1.P2 produced.
 alpha, azimuth of the geodesic at the equator.
-u^2 = (cos(alpha))^2 * (a^2 - b^2)/b^2.
+u ** 2 = (cos(alpha)) ** 2 * (a ** 2 - b ** 2)/b ** 2.
 redlat, reduced latitude, defined by tan(redlat)= (1-f) * tan(phi).
 longdiff_sphere, difference in longilude on an auxiliary sphere.
 sigma, angular distance P1.P2 on the sphere.
@@ -39,25 +39,25 @@ def dist(long_1, long_2, lat_1, lat_2, a, b, f):
     # initial guess
     longdiff_sphere = longdiff
     
-    while(1):
+    while 1:
         longdiff_sphere_0 = longdiff_sphere
         sin_longdiff_sphere = np.sin(longdiff_sphere)
         cos_longdiff_sphere = np.cos(longdiff_sphere)
-        sin_sigma = np.sqrt((cos_redlat_2 * sin_longdiff_sphere)^2 + (cos_redlat_1 * sin_redlat_2 - sin_redlat_1 * cos_redlat_2 * cos_longdiff_sphere)^2)
+        sin_sigma = np.sqrt((cos_redlat_2 * sin_longdiff_sphere) ** 2 + (cos_redlat_1 * sin_redlat_2 - sin_redlat_1 * cos_redlat_2 * cos_longdiff_sphere) ** 2)
         cos_sigma = sin_redlat_1 * sin_redlat_2 + cos_redlat_1 * cos_redlat_2 * cos_longdiff_sphere
         # tan_sigma = sin_sigma / cos_sigma
         sigma = np.acos(cos_sigma)
         sin_alpha = cos_redlat_1 * cos_redlat_2 * sin_longdiff_sphere / sin_sigma
-        cos_2sigma_m = cos_sigma - 2 * sin_redlat_1 * sin_redlat_2 / (1 - (sin_alpha^2))
-        ccc = (f / 16) * (1 - (sin_alpha^2)) * (4 + f * (4 - 3 * (1 - (sin_alpha^2))))
-        longdiff_sphere = longdiff + (1 - ccc) * f * sin_alpha * (sigma + ccc * sin_sigma * (cos_2sigma_m + ccc * cos_sigma * (-1 + 2 * (cos_2sigma_m^2))))
-        if np.absolute(longdiff_sphere - longdiff_sphere_0) < 10^(-12):
+        cos_2sigma_m = cos_sigma - 2 * sin_redlat_1 * sin_redlat_2 / (1 - (sin_alpha ** 2))
+        ccc = (f / 16) * (1 - (sin_alpha ** 2)) * (4 + f * (4 - 3 * (1 - (sin_alpha ** 2))))
+        longdiff_sphere = longdiff + (1 - ccc) * f * sin_alpha * (sigma + ccc * sin_sigma * (cos_2sigma_m + ccc * cos_sigma * (-1 + 2 * (cos_2sigma_m ** 2))))
+        if np.absolute(longdiff_sphere - longdiff_sphere_0) < 10 ** (-12):
             break
 
-        u_squared = (1 - sin_alpha^2) * (a^2 - b^2) / b^2
+        u_squared = (1 - sin_alpha ** 2) * (a ** 2 - b ** 2) / b ** 2
         aaa = 1 + (u_squared / 16384) * (4096 + u_squared * (-768 + u_squared * (320 - 175 * u_squared)))
         bbb = (u_squared / 1024) * (256 + u_squared * (-128 + u_squared * (74 - 47 * u_squared)))
-        del_sigma = bbb * sin_sigma * (cos_2sigma_m + (bbb / 4) * cos_sigma * (-1 + 2 * cos_2sigma_m^2) - (bbb / 6) * cos_2sigma_m * (-3 + 4 * sin_sigma^2) * (-3 + 4 * cos_2sigma_m^2))
+        del_sigma = bbb * sin_sigma * (cos_2sigma_m + (bbb / 4) * cos_sigma * (-1 + 2 * cos_2sigma_m ** 2) - (bbb / 6) * cos_2sigma_m * (-3 + 4 * sin_sigma ** 2) * (-3 + 4 * cos_2sigma_m ** 2))
         
         # finally, the distance:
         s = b * aaa * (sigma - del_sigma)
