c2b526f1268b4bc503598e830aa64f9de4bd74c2
abruns123 <aabruns123@gmail.com>
2024-10-24 22:55:12 +0000

    adjusted motion_direction to account for noise

--- main_file/motion_direction.py
+++ main_file/motion_direction.py
@@ -4,34 +4,50 @@ import pandas as pd
 def get_del_t(times):
     dt=[]
     for i in range(len(times)-1):
-        dt.append([times[i+1]-times[i]])
+        dt.append(times[i+1]-times[i])
     sum=0
     for i in dt:
         sum+=i
     sum=sum/len(dt)
     dt.append(sum)
     return dt
+def adjust_acc(acc):
+    for i in range(len(acc)):
+        if np.abs(acc[i])<2:
+            acc[i]=0
+    print(acc)
+    return acc
 
-def get_vel(a, dt, v, i):
-    if i==0:
-        return v
-    return v+get_vel(a,dt,a[i-1]*dt[i-1],i-1)
+def get_vel(a, dt, i):
+    vel=0
+    for j in range(i+1):
+        vel+=a[j]*dt[j]
+    return vel
 
 
 def get_direction(times, acc):
     dt=get_del_t(times)
+    acc=adjust_acc(acc)
     vel=[]
     for i in range(len(dt)):
-        vel.append(get_vel(acc,dt, acc[i]*dt[i],i))
+        vel.append(get_vel(acc,dt,i))
 
     direction=[]
     for i in range(len(vel)):
-        direction.append(vel[i]/(np.abs(vel[i])))
+        if vel[i]!=0:
+            direction.append(vel[i]/(np.abs(vel[i])))
+        else:
+            direction.append(0)
 
     return direction
 
 def acc_reader(path):
     file =pd.read_csv(path)
-    print(file)
+    acc=file["Linear Acceleration z (m/s^2)"]
+    times=file["Time (s)"]
+    return acc, times
+    
+acc,times=acc_reader("/workspaces/CP1-24-midterm/main_file/up_data/Raw Data.csv")
+
 
     
