bb4d60d5fd40306c364629e7fa9dd5ffb714a760
Patrick Pampo <patrickpampo@gmail.com>
2024-10-24 18:25:14 -0400

    fixed upper case naming issue in mtaccelmain

--- p-pxmpo/p-pxmpoMidtermCode/acceleration/mtaccelmain.py
+++ p-pxmpo/p-pxmpoMidtermCode/acceleration/mtaccelmain.py
@@ -32,5 +32,5 @@ file_input = input("Please enter the CSV file name (including .csv extension): "
 if not os.path.isfile(file_input):
     print(f"Error: The file '{file_input}' was not found.")
 else:
-    acceleration_direction = calculate_dir(file_input)
-    print("The direction of acceleration is", acceleration_direction)
+    direction_of_acceleration  = calculate_dir(file_input)
+    print("The direction of acceleration is", direction_of_acceleration )
