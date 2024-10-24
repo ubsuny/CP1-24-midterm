231f0f280ed92c7b44249f555a0ed5d7ba22f92d
Priyanka_43 <PRIYANK3@BUFFALO.EDU>
2024-10-24 16:50:40 +0000

    Added docstrings.

--- Priyanka_43/Code/unit_converter.py
+++ Priyanka_43/Code/unit_converter.py
@@ -1,17 +1,26 @@
+"""
+This module converts feet and yards to meter
+"""
+
 # unit_converter.py
 
 def feet_to_meters(feet):
     """
     Convert feet to meters.
+    Parameters:
+    feet (float): The distance in feet.
+    Returns:
+    float: The distance in meters.
     """
-    if feet < 0:
-        raise ValueError("Feet value cannot be negative.")
     return feet * 0.3048
+
 
 def yards_to_meters(yards):
     """
     Convert yards to meters.
+    Parameters:
+    yard (float): The distance in yard.
+    Returns:
+    float: The distance in meters.
     """
-    if yards < 0:
-        raise ValueError("Yards value cannot be negative.")
     return yards * 0.9144
