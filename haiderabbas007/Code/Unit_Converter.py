78f6acfa3d5a633e74cf653ea6e5e0d8c9cb549d
Muhammad Haider Abbas <157842721+haiderabbas007@users.noreply.github.com>
2024-10-24 20:09:23 -0400

    Update and rename Code to Unit_Converter.py

---
+++ haiderabbas007/Code/Unit_Converter.py
@@ -0,0 +1,10 @@
+"""
+This module converts lengths in yards or feet to meters.
+"""
+
+def unit_converter(val, unit):
+  """
+  Takes in lengths in yards or feet and converts to meters.
+
+  Parameters:
+      val (float):
