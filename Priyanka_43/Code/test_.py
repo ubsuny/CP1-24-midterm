5c10912c08c548673cfc12c9fb7382b983ed2b15
Priyanka_43 <PRIYANK3@BUFFALO.EDU>
2024-10-25 19:24:34 -0400

    Update and rename test_*.py to test_.py
    Corrected error suggested by pylint.

--- Priyanka_43/Code/test_.py
+++ Priyanka_43/Code/test_.py
@@ -3,19 +3,22 @@ unit testing module for unit_converter.py
 '''
 import pytest
 
-from unit_converter import feet_to_metres, yards_to_metres
+from unit_converter import feet_to_meters, yards_to_meters
 
-def test_feet_to_metres():
+from motion_direction import calculate_direction
+
+def test_feet_to_meters():
     '''
-    function to test the foot to metre conversion
+    function to test the foot to meter conversion
     '''
-    assert feett_to_metres(1) == 0.3048   # checking if 1 foot = 0.3048 metre or not
+    assert feett_to_meters(1) == 0.3048   # checking if 1 foot = 0.3048 meter or not
   
-def test_yards_to_metres():
+def test_yards_to_meters():
     '''
-    function to test the yards to metres conversion
+    function to test the yards to meters conversion
     '''
-    assert yards_to_metres(1) == 0.9144   # checking if 1 yard = 0.9144 metre or not
+    assert yards_to_meters(1) == 0.9144   # checking if 1 yard = 0.9144 meter or not
+
 
 '''
 unit testing module for motion_direction.py
