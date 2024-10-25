6fa8c0750ee196076544b933dfacd49eeaf7e00b
abruns123 <aabruns123@gmail.com>
2024-10-25 16:18:15 +0000

    syncing

--- main_file/direction_of_motion1.py
+++ main_file/direction_of_motion1.py
@@ -2,16 +2,32 @@ import numpy as np
 import pandas as pd
 
 def get_del_t(times):
+    """
+    get_del_t goes through a list of times (times)
+    and determines the difference between each 
+    sequential time. It returns a list of these
+    differences.
+    """
+    #The later time is subtracted from the initial time
+    #for each set of sequential times in the times list
+    #to produce the dt list.
     dt=[]
     for i in range(len(times)-1):
         dt.append(times[i+1]-times[i])
+
+    #To keep dt the same length as times, the average dt
+    #value is appended as the last value. 
     sum=0
     for i in dt:
         sum+=i
     sum=sum/len(dt)
     dt.append(sum)
     return dt
+
 def adjust_acc(acc):
+    """
+    adjust_acc()
+    """
     for i in range(len(acc)):
         if np.abs(acc[i])<10:
             acc[i]=0
