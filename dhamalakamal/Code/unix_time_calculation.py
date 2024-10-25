28ddb1ebf66ea5f883e7095b713af0ef171586fd
dhamalakamal <155951211+dhamalakamal@users.noreply.github.com>
2024-10-24 22:45:51 -0400

    python module for Unix time calculation for and its importable functions

---
+++ dhamalakamal/Code/unix_time_calculation.py
@@ -0,0 +1,8 @@
+from unix_time import read_md_file_and_convert_to_unix
+
+MD_FILE = r'D:\Projects\CP1-24-midterm\dhamalakamal\Documentation\KD_acceleration.md'
+unix_time = read_md_file_and_convert_to_unix(MD_FILE)
+if unix_time is not None:
+    print(f"Unix time: {unix_time}")
+else:
+    print("No valid date found in the markdown file.")
