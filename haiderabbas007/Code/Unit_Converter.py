498469d768ca5d42a0f1eebb25e9d36bc59d7b8a
Muhammad Haider Abbas <157842721+haiderabbas007@users.noreply.github.com>
2024-10-24 20:21:31 -0400

    Update Unit_Converter.py

--- haiderabbas007/Code/Unit_Converter.py
+++ haiderabbas007/Code/Unit_Converter.py
@@ -7,4 +7,17 @@ def unit_converter(val, unit):
   Takes in lengths in yards or feet and converts to meters.
 
   Parameters:
-      val (float):
+      val (float): magnitude of the length.
+      unit (str): unit of length ('ft' or 'yd').
+
+  Returns:
+      float: Length converted to meters.
+  """
+  if unit == 'ft':
+    val = val * 0.3048  # From Google Unit Converter
+    return val
+  elif unit == 'yd':
+    val = val * 0.9144  # From Google Unit Converter
+    return val
+  else:
+    raise ValueError("Only 'ft' or 'yd' please.)
