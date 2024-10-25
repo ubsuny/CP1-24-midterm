0dc923dbaf9804ad0c90999d273d57ae2a245d76
dhamalakamal <155951211+dhamalakamal@users.noreply.github.com>
2024-10-25 04:02:53 +0000

    Added docstring at the front part of python module that converts feet and yards to metres.

--- dhamalakamal/Code/feet_to_meter.py
+++ dhamalakamal/Code/feet_to_meter.py
@@ -1,5 +1,13 @@
 """
-This module converts feet and yard to metre.
+This module provides simple conversion functions to convert distances from feet and yards to meters.
+
+Constants:
+    - FEET_TO_METERS: Conversion factor from feet to meters (1 foot = 0.3048 meters).
+    - YARDS_TO_METERS: Conversion factor from yards to meters (1 yard = 0.9144 meters).
+
+Functions:
+    - feet_to_meters(feet): Converts a given distance in feet to meters.
+    - yards_to_meters(yards): Converts a given distance in yards to meters.
 """
 # Conversion factors
 FEET_TO_METERS = 0.3048
