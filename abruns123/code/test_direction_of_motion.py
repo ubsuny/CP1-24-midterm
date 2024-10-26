881aaa40725948cec15525422e64896061a2c8cc
abruns123 <aabruns123@gmail.com>
2024-10-26 02:48:30 +0000

    adjusted csv file references again

--- abruns123/code/test_direction_of_motion.py
+++ abruns123/code/test_direction_of_motion.py
@@ -8,12 +8,12 @@ def test_direction():
     test_direction verifies that the start and end behaviors
     of the two elevator experiments are as expected
     """
-    acc,times=acc_reader("/abruns123/data/down_data/Raw Data.csv")
+    acc,times=acc_reader("/workspaces/CP1-24-midterm/abruns123/data/down_data/Raw Data.csv")
     directions=get_direction(times, acc)
     assert directions[0]==0
     assert directions[len(directions)-1]==0
 
-    acc,times=acc_reader("/abruns123/data/up_data/Raw Data.csv")
+    acc,times=acc_reader("/workspaces/CP1-24-midterm/abruns123/data/up_data/Raw Data.csv")
     directions=get_direction(times, acc)
     assert directions[0]==0
     assert directions[len(directions)-1]==0
