84edc95c41e4ee933ec30228ba296b2adf1813b8
abruns123 <aabruns123@gmail.com>
2024-10-26 02:30:15 +0000

    added correct test files

---
+++ abruns123/code/test_direction_of_motion.py
@@ -0,0 +1,19 @@
+"""
+This module tests the direction_of_motion module
+"""
+from direction_of_motion import get_direction, acc_reader
+
+def test_direction():
+    """
+    test_direction verifies that the start and end behaviors
+    of the two elevator experiments are as expected
+    """
+    acc,times=acc_reader("/AB123/data/down_data/Raw Data.csv")
+    directions=get_direction(times, acc)
+    assert directions[0]==0
+    assert directions[len(directions)-1]==0
+
+    acc,times=acc_reader("/AB123/data/up_data/Raw Data.csv")
+    directions=get_direction(times, acc)
+    assert directions[0]==0
+    assert directions[len(directions)-1]==0
---
+++ abruns123/code/test_unix_time.py
@@ -0,0 +1,42 @@
+"""
+This module tests the unix_time module
+"""
+from unix_time import years, time, time_reader,date_reader
+
+def test_years():
+    """
+    test_years verifies that the 
+    years function can accurately
+    create lists of length corresponding
+    to the number of leap years and 
+    non-leap years between 1970 and the 
+    input year. 
+    """
+    ny,ly=years(2130)
+    assert len(ny)==121
+    assert len(ly)==39
+
+def test_time():
+    """
+    test_time uses a known unix time 
+    taken from the unix timestamp
+    website and verifies that
+    the time function calculates the correct
+    unix time.
+    """
+    #Test is based on comparison with test value used 
+    #on unix timestamp website
+    #citation: [1][1] Unix Time Stamp - Epoch Converter. (n.d.). 
+    # https://www.unixtimestamp.com/
+    date=[2024,10,24,17,45,53]
+    assert time(date)==1729806353
+
+def test_reader():
+    """
+    test reader verifies that the various reader functions 
+    can run without producing an error in the case that a 
+    file without the data they're meant to read is the input.
+    """
+    time_reader("/AB123/data/Triangle Data/time.csv")
+    date_reader("/AB123/data/Triangle Data/Location.csv")
+    
